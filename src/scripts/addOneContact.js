c209c0f12c1432709196c3e113d7837518ff7c47
Kate <kate.dovzhenk@gmail.com>
2025-05-11 17:17:48 -0400

    syntax error was fix

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,11 +1,11 @@
-import { createFakeContacts } from '../utils/createFakeContact.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 export const addOneContact = async () => {
   try {
     const contacts = await readContacts();
-    const newContact = createFakeContacts();
+    const newContact = createFakeContact();
     const updatedContacts = [...contacts, newContact];
     await writeContacts(updatedContacts);
     console.log('Один контакт успішно додано.');
